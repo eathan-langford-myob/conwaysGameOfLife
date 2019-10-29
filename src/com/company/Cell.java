59c0b5a39fdad3ef30fb23835da6fa26b4c34006
Eathan Langford <eathan.langford@myob.com>
2019-10-30 10:15:20 +1100

    first commit, solved test for new Cell

---
+++ src/com/company/Cell.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Cell {
+   private boolean isAlive;
+
+    public Cell() {
+        this.isAlive = false;
+    }
+
+    public Cell(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    public boolean isAlive(){
+    return isAlive;
+}
+
+}
